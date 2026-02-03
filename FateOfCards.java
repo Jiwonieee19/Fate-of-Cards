ba7c0b997f7094c5b76f10bf89497a83fb8d72f6
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-02-03 23:16:50 +0800

    slow progress, structure tempo

--- FateOfCards.java
+++ FateOfCards.java
@@ -1,15 +1,42 @@
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FateOfCards extends JPanel {
+public class FateOfCards extends JPanel implements ActionListener {
 
     int width = 700;
     int height = 900;
+
+    class cards {
+        int x, y, width, height;
+        Image img;
+        Color standard = Color.WHITE;
+
+        cards(int x, int y, int width, int height, Image img) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+        }
+    }
+
+    Timer timer;
 
     FateOfCards() {
         setPreferredSize(new Dimension(width, height));
         setFocusable(true);
         setBackground(Color.BLACK);
+
+        timer = new Timer(1000 / 10, this); // 10 fps
+        timer.start();
+
+    }
+
+    public void move() {
+
     }
 
     public void paintComponent(Graphics g) {
@@ -18,6 +45,13 @@ public class FateOfCards extends JPanel {
     }
 
     public void draw(Graphics g) {
-        // cc cc cc
+        g.setColor(Color.WHITE);
+        g.drawString("Fate of Cards", width / 2 - 30, 20);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
     }
 }
