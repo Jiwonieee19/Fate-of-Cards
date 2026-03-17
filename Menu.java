68b65be16a33d6cc41d183ef001e9791e155b7d6
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-03-17 23:54:23 +0800

    in the middle of making menu whether a jpanel or directly in mainjframe

--- Menu.java
+++ Menu.java
@@ -11,7 +11,9 @@ public class Menu extends JPanel implements ActionListener {
     int width = 700;
     int height = 900;
 
-    JButton butt;
+    JButton casualButton;
+
+    FateOfCards Foc;
 
     Menu() {
 
@@ -20,25 +22,33 @@ public class Menu extends JPanel implements ActionListener {
         setBackground(Color.BLACK);
         // addMouseListener(this);
 
-        butt = new JButton();
+        casualButton = new JButton();
 
-        butt.setFont(new Font("Arial", Font.BOLD, 24));
-        butt.setText("Casual Game");
-        butt.setFocusable(false);
-        butt.addActionListener(this);
-        butt.setEnabled(true);
-        butt.setVisible(true);
-        butt.setBackground(Color.BLACK);
-        butt.setBorderPainted(false);
+        casualButton.setFont(new Font("Arial", Font.BOLD, 24));
+        casualButton.setText("Casual Game");
+        casualButton.setFocusable(false);
+        casualButton.addActionListener(this);
+        casualButton.setEnabled(true);
+        casualButton.setVisible(true);
+        casualButton.setBackground(Color.BLACK);
+        casualButton.setBorderPainted(false);
+        // mag flick lang ang effect if clicked, nays ilhanan
+        // casualButton.setContentAreaFilled(false);
+        // goods no bg, but walay aftereffect if gi click
+        // casualButton.setBackground(new Color(0, 0, 0, 0));
+        // naay effect pag click but mag stay na pud in ato forever
 
-        this.add(butt);
+        this.add(casualButton);
 
+        Foc = new FateOfCards();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == butt) {
+        if (e.getSource() == casualButton) {
             System.out.println("Hi");
+            this.add(Foc);
+            Foc.setVisible(true);
         }
     }
 }
--- myFrame.java
+++ myFrame.java
@@ -1,26 +1,54 @@
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class myFrame extends JFrame {
+public class myFrame extends JFrame implements ActionListener {
 
-    int width = 100;
-    int height = 100;
+    int width = 700;
+    int height = 900;
+
+    JButton casualButton;
+
+    FateOfCards Foc = new FateOfCards();
 
     myFrame() {
         setTitle("FoC");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(width, height);
         setResizable(false);
+        setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
 
-        // FateOfCards FoC = new FateOfCards();
-        Menu menu = new Menu();
+        // Menu menu = new Menu();
 
         // FoC.requestFocus();
         // add(FoC);
         // pack();
 
-        menu.requestFocus();
-        add(menu);
-        pack();
-        setLocationRelativeTo(null);
+        // menu.requestFocus();
+        // add(menu);
+        // pack();
+
+        casualButton = new JButton();
+        casualButton.setFont(new Font("Arial", Font.BOLD, 24));
+        casualButton.setText("Hi");
+        casualButton.setSize(25, 40);
+        casualButton.setFocusable(false);
+        casualButton.setEnabled(true);
+        this.add(casualButton);
+
+        add(Foc);
+        Foc.setVisible(false);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == casualButton) {
+            Foc.requestFocus();
+            Foc.setVisible(true);
+        }
     }
 }
